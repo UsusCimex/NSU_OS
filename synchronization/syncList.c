bdc1da4485f83f50fd8d5131e9b7eddc1393ea1a
UsusCimex <d.lanin@g.nsu.ru>
2023-12-02 15:21:43 +0700

    fix racecondition and deadlock situation

--- synchronization/syncList.c
+++ synchronization/syncList.c
@@ -19,6 +19,8 @@ int decreasing_length_count = 0;
 int equal_length_count = 0;
 int swap_count = 0;
 
+pthread_mutex_t global_mutex;
+
 void addNode(Storage* storage, const char* value) {
     Node* new_node = (Node* )malloc(sizeof(Node));
     strncpy(new_node->value, value, 100);
@@ -51,17 +53,20 @@ void* countIncreasingLengthPairs(void* arg) {
     Storage* storage = (Storage*)arg;
     while (1) {
         Node* current = storage->first;
-        while (current != NULL && current->next != NULL) {
+        Node* next = NULL;
+
+        while (current != NULL) {
             pthread_mutex_lock(&current->sync);
-            pthread_mutex_lock(&current->next->sync);
-
-            if (strlen(current->value) < strlen(current->next->value)) {
-                increasing_length_count++;
+            next = current->next;
+            if (next != NULL) {
+                pthread_mutex_lock(&next->sync);
+                if (strlen(current->value) < strlen(next->value)) {
+                    increasing_length_count++;
+                }
+                pthread_mutex_unlock(&next->sync);
             }
-
-            pthread_mutex_unlock(&current->next->sync);
             pthread_mutex_unlock(&current->sync);
-            current = current->next;
+            current = next;
         }
     }
     return NULL;
@@ -72,17 +77,20 @@ void* countDecreasingLengthPairs(void* arg) {
     Storage* storage = (Storage* )arg;
     while (1) {
         Node* current = storage->first;
-        while (current != NULL && current->next != NULL) {
+        Node* next = NULL;
+
+        while (current != NULL) {
             pthread_mutex_lock(&current->sync);
-            pthread_mutex_lock(&current->next->sync);
-
-            if (strlen(current->value) > strlen(current->next->value)) {
-                decreasing_length_count++;
+            next = current->next;
+            if (next != NULL) {
+                pthread_mutex_lock(&next->sync);
+                if (strlen(current->value) > strlen(next->value)) {
+                    decreasing_length_count++;
+                }
+                pthread_mutex_unlock(&next->sync);
             }
-
-            pthread_mutex_unlock(&current->next->sync);
             pthread_mutex_unlock(&current->sync);
-            current = current->next;
+            current = next;
         }
     }
     return NULL;
@@ -93,99 +101,86 @@ void* countEqualLengthPairs(void* arg) {
     Storage* storage = (Storage*)arg;
     while (1) {
         Node* current = storage->first;
-        while (current != NULL && current->next != NULL) {
+        Node* next = NULL;
+
+        while (current != NULL) {
             pthread_mutex_lock(&current->sync);
-            pthread_mutex_lock(&current->next->sync);
-
-            if (strlen(current->value) == strlen(current->next->value)) {
-                equal_length_count++;
+            next = current->next;
+            if (next != NULL) {
+                pthread_mutex_lock(&next->sync);
+                if (strlen(current->value) == strlen(next->value)) {
+                    equal_length_count++;
+                }
+                pthread_mutex_unlock(&next->sync);
             }
-
-            pthread_mutex_unlock(&current->next->sync);
             pthread_mutex_unlock(&current->sync);
-            current = current->next;
+            current = next;
         }
     }
     return NULL;
 }
 
-void swapNodes(Storage *storage, Node *node1, Node *node2) {
-    // Проверяем, что узлы не NULL и не одинаковые
-    if (node1 == NULL || node2 == NULL || node1 == node2) {
-        return;
+void swapNodes(Storage* storage, Node* node1, Node* node2) {
+    if (node1 == NULL || node2 == NULL || node1 == node2) return;
+
+    // Находим предшествующие узлы для node1 и node2
+    Node *prev1 = NULL, *prev2 = NULL, *curr = storage->first;
+    while (curr != NULL && (prev1 == NULL || prev2 == NULL)) {
+        if (curr->next == node1) prev1 = curr;
+        if (curr->next == node2) prev2 = curr;
+        curr = curr->next;
     }
 
-    // Находим узел, предшествующий node1
-    Node *prev1 = NULL;
-    Node *current = storage->first;
-    while (current != NULL && current->next != node1) {
-        prev1 = current;
-        current = current->next;
-    }
+    // Обновляем предшествующие узлы
+    if (prev1 != NULL) prev1->next = node2;
+    if (prev2 != NULL) prev2->next = node1;
 
-    // Находим узел, предшествующий node2
-    Node *prev2 = NULL;
-    current = storage->first;
-    while (current != NULL && current->next != node2) {
-        prev2 = current;
-        current = current->next;
-    }
-
-    // Если node1 или node2 не найдены, возвращаемся
-    if (current == NULL) {
-        return;
-    }
-
-    // Меняем узлы местами
-    if (prev1 != NULL) {
-        prev1->next = node2;
-    } else {
-        storage->first = node2;
-    }
-
-    if (prev2 != NULL) {
-        prev2->next = node1;
-    } else {
-        storage->first = node1;
-    }
-
-    // Обмен указателями next
-    Node *temp = node1->next;
+    // Обмениваем следующие узлы
+    Node* temp = node1->next;
     node1->next = node2->next;
     node2->next = temp;
+
+    // Обновляем первый элемент списка, если необходимо
+    if (storage->first == node1) storage->first = node2;
+    else if (storage->first == node2) storage->first = node1;
 }
 
 // Функция потока для перестановки узлов списка
 void* swapNodesInList(void* arg) {
-    Storage* storage = (Storage* )arg;
+    Storage* storage = (Storage*)arg;
     while (1) {
         Node* current = storage->first;
-        while (current != NULL && current->next != NULL) {
+        Node* next = NULL;
+
+        while (current != NULL) {
+            pthread_mutex_lock(&global_mutex); // To avoid deadlock
             pthread_mutex_lock(&current->sync);
-            pthread_mutex_lock(&current->next->sync);
-
-            // Логика перестановки узлов?
-            if (rand() % 100 < 25) { // 25% to swap
-                swapNodes(storage, current, current->next);
+            next = current->next;
+            if (next != NULL) {
+                pthread_mutex_lock(&next->sync);
+                if (next != NULL && rand() % 100 < 25) { // 25% to swap
+                    swapNodes(storage, current, next);
+                    swap_count++;
+                }
+                pthread_mutex_unlock(&next->sync);
             }
-
-            pthread_mutex_unlock(&current->next->sync);
             pthread_mutex_unlock(&current->sync);
-            current = current->next;
+            current = next;
+            pthread_mutex_unlock(&global_mutex);
         }
     }
     return NULL;
 }
 
-void* monitor() {
+void* monitor(void* arg) {
     while (1) {
-        sleep(1);
-        printf("increasing_length_count = %d\ndecreasing_length_count = %d\nequal_length_count = %d\nswap_count = %d",
+        printf("increasing_length_count = %d\ndecreasing_length_count = %d\nequal_length_count = %d\nswap_count = %d\n",
                 increasing_length_count,
                 decreasing_length_count,
                 equal_length_count,
                 swap_count);
-    }    
+        sleep(1);
+    }
 }
 
 void generateRandomString(char *str, int length) {
@@ -210,20 +205,24 @@ int main() {
     Storage storage;
     storage.first = NULL;
 
-    generateList(&storage, 100);
+    generateList(&storage, 10000);
 
-    pthread_t inc_thread, dec_thread, eq_thread, swap_thread, monitor_thread;
+    pthread_t inc_thread, dec_thread, eq_thread, swap_thread1, swap_thread2, swap_thread3, monitor_thread;
     pthread_create(&inc_thread, NULL, countIncreasingLengthPairs, (void* )&storage);
     pthread_create(&dec_thread, NULL, countDecreasingLengthPairs, (void* )&storage);
     pthread_create(&eq_thread, NULL, countEqualLengthPairs, (void* )&storage);
-    pthread_create(&swap_thread, NULL, swapNodesInList, (void* )&storage);
+    pthread_create(&swap_thread1, NULL, swapNodesInList, (void* )&storage);
+    pthread_create(&swap_thread2, NULL, swapNodesInList, (void* )&storage);
+    pthread_create(&swap_thread3, NULL, swapNodesInList, (void* )&storage);
     pthread_create(&monitor_thread, NULL, monitor, (void* )&storage);
 
     // Дождаться завершения потоков (в данном случае, потоки выполняются бесконечно)
     pthread_join(inc_thread, NULL);
     pthread_join(dec_thread, NULL);
     pthread_join(eq_thread, NULL);
-    pthread_join(swap_thread, NULL);
+    pthread_join(swap_thread1, NULL);
+    pthread_join(swap_thread2, NULL);
+    pthread_join(swap_thread3, NULL);
     pthread_join(monitor_thread, NULL);
 
     return 0;
